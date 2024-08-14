64104fe6e6202ea30de72d852522457ffde754db
Anthony Vásquez <120955350+Thony764@users.noreply.github.com>
2024-08-13 21:36:22 -0500

    Main.java

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,6 +3,8 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.setVisible(true);
     }
 
 }
